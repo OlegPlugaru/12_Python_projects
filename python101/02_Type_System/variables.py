6d59199230effcd16fb0599f783309d2c588104f
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-02-08 22:01:28 +0200

    added some comments

--- python101/02_Type_System/variables.py
+++ python101/02_Type_System/variables.py
@@ -1,6 +1,15 @@
+# ----------------------------------------------------------------
+# Louis is going for a party, he has got balloons, crackers & hats
+# ----------------------------------------------------------------
+
+# Declaring a new variable called as 'box'
 box = 'balloons'
-print(box)
+print(box) # prints -> Balloons
+
+# Louis changes his mind, he now wants to pack crackers instead
 box = 'crackers'
-print(box)
+print(box) # print -> Crackers
+
+# Louis changes his mind yet again, now wants to pack hats
 box = 'hats'
-print(box)
+print(box) # print -> Hats
